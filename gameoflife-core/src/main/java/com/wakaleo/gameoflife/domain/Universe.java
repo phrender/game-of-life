f31f4396e991237da20030854649a640a85852bc
Kevin Berglund <kevteq@gmail.com>
2019-06-08 15:39:35 +0200

    Changed  a file to trigger a rebuild

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
@@ -10,6 +10,7 @@ import static com.wakaleo.gameoflife.domain.Cell.DEAD_CELL;
  * Universe.java
  * A universe is a succession of grids over time.
  * Each new grid is generated from the previous one using the rules of the Game Of Life.
+ * Testign to change a file to trigger a rebuild
  */
 public class Universe {
 
